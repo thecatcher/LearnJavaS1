8549de56227dc7eac5ed612055bd1a099ff422b6
gaoyang <gaoyang1019@hotmail.com>
2017-03-18 23:01:35 +0800

    Delete Test.java
    delete test.java in String

--- String/Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Test{
-	public static void main(String[] args){
-		Collection c = new ArrayList();
-		c.add(new Integer(100));
-		c.add("hello");
-		System.out.println(c);
-	}
-}
